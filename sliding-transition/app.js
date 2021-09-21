b0adf74e49383d4dd0a97754014f463a00dfbf13
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-09-21 15:20:01 +0530

    sliding transition

---
+++ sliding-transition/app.js
@@ -0,0 +1,9 @@
+function change(){
+    if(window.scrollY > window.innerHeight/2){
+        document.body.classList.add("bg-active");
+    }else{
+        document.body.classList.remove("bg-active");
+    }
+}
+
+window.addEventListener("scroll",change);
